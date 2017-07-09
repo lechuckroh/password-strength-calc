59caf0e9e0e65cb328ae336285096c30db9be5bd
Lechuck Roh <member@lechuck2011.cafe24.com>
2017-07-09 18:35:01 +0900

    all rules implemented

--- index.js
+++ index.js
@@ -1,138 +1,274 @@
 'use strict';
 
-const charList = s => s.split('');
+const alphaRe = /[a-zA-Z]/;
+const uppercaseRe = /[A-Z]/;
+const lowercaseRe = /[a-z]/;
+const numericRe = /[0-9]/;
+const symbolRe = /[^a-zA-Z0-9]/;
+const numericSymbolRe = /[^a-zA-Z]/;
+
+const charList = s => Array.isArray(s) ? s : s.split('');
+const toString = s => Array.isArray(s) ? s.join('') : s;
+const isAlpha = ch => alphaRe.test(ch);
+const isUppercase = ch => uppercaseRe.test(ch);
+const isLowercase = ch => lowercaseRe.test(ch);
+const isNumeric = ch => numericRe.test(ch);
+const isSymbol = ch => symbolRe.test(ch);
+const isNumericSymbol = ch => numericSymbolRe.test(ch);
+
+const numberSeq = '01234567890';
+const alphaSeq = 'abcdefghijklmnopqrstuvwxyz';
+const symbolSeq = '~!@#$%^&*()_+{}|<>?,./';
 
 const functions = {
-    calcNumberOfChars: function (password) {
-        return password.length * 4;
+    calcNumberOfChars: function (pwd) {
+        return pwd.length * 4;
     },
 
     /**
      * Check count of uppercase character.
      * non-uppercase character required to get score.
      */
-    calcUppercaseLetters: function (password) {
-        const count = charList(password).reduce((count, ch) => {
-            return count + (ch.match(/[A-Z]/g) ? 1 : 0);
+    calcUppercaseLetters: function (pwd) {
+        const count = charList(pwd).reduce((count, ch) => {
+            return isUppercase(ch) ? count + 1 : count;
         }, 0);
-        const length = password.length;
-        return (count < length) ? (length - count) * 2 : 0;
+        const length = pwd.length;
+        return (count && count < length) ? (length - count) * 2 : 0;
     },
 
     /**
      * Check count of lowercase character.
      * non-lowercase character required to get score.
      */
-    calcLowercaseLetters: function (password) {
-        const count = charList(password).reduce((count, ch) => {
-            return count + (ch.match(/[a-z]/g) ? 1 : 0);
+    calcLowercaseLetters: function (pwd) {
+        const count = charList(pwd).reduce((count, ch) => {
+            return isLowercase(ch) ? count + 1 : count;
         }, 0);
-        const length = password.length;
-        return (count < length) ? (length - count) * 2 : 0;
+        const length = pwd.length;
+        return (count && count < length) ? (length - count) * 2 : 0;
     },
 
     /**
      * Check count of numbers.
      * non-numeric character required to get score.
      */
-    calcNumbers: function (password) {
-        const count = charList(password).reduce((count, ch) => {
-            return count + (ch.match(/[0-9]/g) ? 1 : 0);
+    calcNumbers: function (pwd) {
+        const count = charList(pwd).reduce((count, ch) => {
+            return isNumeric(ch) ? count + 1 : count;
         }, 0);
-        const length = password.length;
+        const length = pwd.length;
         return (count < length) ? count * 4 : 0;
     },
 
     /** Check count of symbols. */
-    calcSymbols: function (password) {
-        const count = charList(password).reduce((count, ch) => {
-            if (ch.match(/[^a-zA-Z0-9]/g)) {
-                count++;
-            }
-            return count;
+    calcSymbols: function (pwd) {
+        const count = charList(pwd).reduce((count, ch) => {
+            return isSymbol(ch) ? count + 1 : count;
         }, 0);
         return count * 6;
     },
 
-    calcMiddleNumbersOrSymbols: function (password) {
-        // TODO:
-        return 0;
+    /** Check count of numbers & symbols in the middle of string */
+    calcMiddleNumbersOrSymbols: function (pwd) {
+        const list = charList(pwd).slice(1, pwd.length - 1);
+        const count = list.reduce((count, ch) => {
+            return isNumericSymbol(ch) ? count + 1 : count;
+        }, 0);
+        return count * 2;
     },
 
-    calcRequirements: function (password) {
-        // TODO
-        return 0;
+    /**
+     * Check minimum requirements.
+     * * Minimum 8 characters in length
+     * * Contains 3/4 of the following items:
+     *   - Uppercase Letters
+     *   - Lowercase Letters
+     *   - Numbers
+     *   - Symbols
+     */
+    calcRequirements: function (pwd) {
+        let hasLowerCase = false;
+        let hasUpperCase = false;
+        let hasNumber = false;
+        let hasSymbol = false;
+        charList(pwd).forEach(ch => {
+            if (isLowercase(ch)) {
+                hasLowerCase = true;
+            } else if (isUppercase(ch)) {
+                hasUpperCase = true;
+            } else if (isNumeric(ch)) {
+                hasNumber = true;
+            } else if (isSymbol(ch)) {
+                hasSymbol = true;
+            }
+        });
+
+        const req1 = (pwd.length >= 8) ? 1 : 0;
+        const req2a = hasLowerCase ? 1 : 0;
+        const req2b = hasUpperCase ? 1 : 0;
+        const req2c = hasNumber ? 1 : 0;
+        const req2d = hasSymbol ? 1 : 0;
+
+        const req2sum = req2a + req2b + req2c + req2d;
+        if (req1 && (req2sum >= 3)) {
+            return (req1 + req2sum) * 2;
+        } else {
+            return 0;
+        }
     },
 
-    calcLettersOnly: function (password) {
-        // TODO
-        return 0;
+    /** Check if every character is a letter */
+    calcLettersOnly: function (pwd) {
+        return charList(pwd).every(ch => isAlpha(ch)) ? -pwd.length : 0;
     },
 
-    calcNumbersOnly: function (password) {
-        // TODO
-        return 0;
+    /** Check if every character is numeric */
+    calcNumbersOnly: function (pwd) {
+        return charList(pwd).every(ch => isNumeric(ch)) ? -pwd.length : 0;
     },
 
-    calcRepeatChars: function (password) {
-        // TODO
-        return 0;
+    /**
+     * Calculate deduction based on proximity to identical characters
+     * Deduction is incremented each time a new match is discovered
+     * Deduction amount is based on total password length divided by the
+     * difference of distance between currently selected match
+     */
+    calcRepeatChars: function (pwd) {
+        const list = charList(pwd);
+        const length = pwd.length;
+
+        let repeatedCharCount = 0;
+        const score = list.reduce((total, c1, i1) => {
+            let inc = 0;
+
+            list.forEach((c2, i2) => {
+                if (i1 !== i2 && c1 === c2) {
+                    inc += Math.abs(length / (i2 - i1));
+                }
+            });
+
+            if (inc > 0) {
+                repeatedCharCount++;
+                const uniqueCharCount = length - repeatedCharCount;
+                total = uniqueCharCount ?
+                    Math.ceil((total + inc) / uniqueCharCount) :
+                    Math.ceil(total + inc);
+            }
+
+            return total;
+        }, 0);
+
+        return -score;
     },
 
-    calcConsecutiveUppercase: function (password) {
-        // TODO
-        return 0;
+    /** Check for consecutive uppercase character */
+    calcConsecutiveUppercase: function (pwd) {
+        const list = charList(pwd);
+        const length = list.length;
+        const count = list.reduce((count, ch, idx, arr) => {
+            if (idx < length - 1) {
+                if (isUppercase(ch) && isUppercase(arr[idx + 1])) {
+                    count++;
+                }
+            }
+            return count;
+        }, 0);
+        return -count * 2;
     },
 
-    calcConsecutiveLowercase: function (password) {
-        // TODO
-        return 0;
+    /** Check for consecutive lowercase character */
+    calcConsecutiveLowercase: function (pwd) {
+        const list = charList(pwd);
+        const length = list.length;
+        const count = list.reduce((count, ch, idx, arr) => {
+            if (idx < length - 1) {
+                if (isLowercase(ch) && isLowercase(arr[idx + 1])) {
+                    count++;
+                }
+            }
+            return count;
+        }, 0);
+        return -count * 2;
     },
 
-    calcConsecutiveNumber: function (password) {
-        // TODO
-        return 0;
+    /** Check for consecutive numeric character */
+    calcConsecutiveNumber: function (pwd) {
+        const list = charList(pwd);
+        const length = list.length;
+        const count = list.reduce((count, ch, idx, arr) => {
+            if (idx < length - 1) {
+                if (isNumeric(ch) && isNumeric(arr[idx + 1])) {
+                    count++;
+                }
+            }
+            return count;
+        }, 0);
+        return -count * 2;
     },
 
-    calcSequentialLetters: function (password) {
-        // TODO
-        return 0;
-    },
-
-    calcSequentialNumbers: function (password) {
-        const numericStr = '01234567890';
+    /** Check for sequential alpha string pattern (forward and backward) */
+    calcSequentialLetters: function (pwd) {
         let count = 0;
-        for (let i = 0; i < 8; i++) {
-            const fwd = numericStr.substring(i, i + 3);
+        const maxIdx = alphaSeq.length - 3;
+        const str = toString(pwd).toLowerCase();
+        for (let i = 0; i <= maxIdx; i++) {
+            const fwd = alphaSeq.substring(i, i + 3);
             const bwd = fwd.split('').reverse().join('');
-            if (password.includes(fwd, bwd)) {
-                console.log('contains', fwd, bwd);
+            if (str.includes(fwd) || str.includes(bwd)) {
                 count++;
             }
         }
-        console.log(-count * 3);
-        return - count * 3;
+        return -count * 3;
     },
 
-    calcSequentialSymbols: function (password) {
-        // TODO
-        return 0;
+
+    /** Check for sequential numeric string pattern (forward and backward) */
+    calcSequentialNumbers: function (pwd) {
+        let count = 0;
+        const maxIdx = numberSeq.length - 3;
+        const str = toString(pwd);
+        for (let i = 0; i <= maxIdx; i++) {
+            const fwd = numberSeq.substring(i, i + 3);
+            const bwd = fwd.split('').reverse().join('');
+            if (str.includes(fwd) || str.includes(bwd)) {
+                count++;
+            }
+        }
+        return -count * 3;
+    },
+
+    /** Check for sequential symbol string pattern (forward and backward) */
+    calcSequentialSymbols: function (pwd) {
+        let count = 0;
+        const maxIdx = symbolSeq.length - 3;
+        const str = toString(pwd);
+        for (let i = 0; i <= maxIdx; i++) {
+            const fwd = symbolSeq.substring(i, i + 3);
+            const bwd = fwd.split('').reverse().join('');
+            if (str.includes(fwd) || str.includes(bwd)) {
+                count++;
+            }
+        }
+        return -count * 3;
     }
 };
 
 /**
  * Get password strength score from 0 to 100
- * @param {string} password 
+ * @param {string} pwd
  */
-function getPasswordStrength(password) {
-    if (!password) {
+function getPasswordStrength(pwd) {
+    if (!pwd) {
         return 0;
     }
 
-    const score = functions.reduce((total, fn) => {
-        total += fn(password);
+    const list = charList(pwd);
+    const score = Object.values(functions).reduce((total, fn) => {
+        total += fn(list);
         return total;
     }, 0);
+
     return Math.max(0, Math.min(100, score));
 }
 
--- test/test.js
+++ test/test.js
@@ -1,61 +1,162 @@
 'use strict';
 
 const assert = require('assert');
-const { getPasswordStrength, functions } = require('../index');
+const {getPasswordStrength, functions} = require('../index');
 
 describe('test', function () {
 
     describe('functions', function () {
         it('calcNumberOfChars', function () {
-            assert.equal(functions.calcNumberOfChars(''), 0);
-            assert.equal(functions.calcNumberOfChars('14'), 8);
-            assert.equal(functions.calcNumberOfChars('abc'), 12);
+            const fn = functions.calcNumberOfChars;
+            assert.equal(fn(''), 0);
+            assert.equal(fn('14'), 8);
+            assert.equal(fn('abc'), 12);
         });
 
-        it('calcUppercaseLetters', function() {
-            assert.equal(functions.calcUppercaseLetters(''), 0);
-            assert.equal(functions.calcUppercaseLetters('AB'), 0);
-            assert.equal(functions.calcUppercaseLetters('ABa'), 2);
-            assert.equal(functions.calcUppercaseLetters('ABCDEaa'), 4);
+        it('calcUppercaseLetters', function () {
+            const fn = functions.calcUppercaseLetters;
+            assert.equal(fn(''), 0);
+            assert.equal(fn('1'), 0);
+            assert.equal(fn('AB'), 0);
+            assert.equal(fn('ABa'), 2);
+            assert.equal(fn('ABCDEaa'), 4);
         });
 
-        it('calcLowercaseLetters', function() {
-            assert.equal(functions.calcLowercaseLetters(''), 0);
-            assert.equal(functions.calcLowercaseLetters('ab'), 0);
-            assert.equal(functions.calcLowercaseLetters('ABa'), 4);
-            assert.equal(functions.calcLowercaseLetters('ABCDEaa'), 10);
+        it('calcLowercaseLetters', function () {
+            const fn = functions.calcLowercaseLetters;
+            assert.equal(fn(''), 0);
+            assert.equal(fn('1'), 0);
+            assert.equal(fn('ab'), 0);
+            assert.equal(fn('ABa'), 4);
+            assert.equal(fn('ABCDEaa'), 10);
         });
 
-        it('calcNumbers', function() {
-            assert.equal(functions.calcNumbers('12'), 0);
-            assert.equal(functions.calcNumbers('a1'), 4);
-            assert.equal(functions.calcNumbers('A12'), 8);
-            assert.equal(functions.calcNumbers('1ab2cd3'), 12);
+        it('calcNumbers', function () {
+            const fn = functions.calcNumbers;
+            assert.equal(fn('12'), 0);
+            assert.equal(fn('a1'), 4);
+            assert.equal(fn('A12'), 8);
+            assert.equal(fn('1ab2cd3'), 12);
         });
 
-        it('calcSymbols', function() {
-            assert.equal(functions.calcSymbols('+'), 6);
-            assert.equal(functions.calcSymbols('Aa1'), 0);
-            assert.equal(functions.calcSymbols('++A++'), 24);
+        it('calcSymbols', function () {
+            const fn = functions.calcSymbols;
+            assert.equal(fn('+'), 6);
+            assert.equal(fn('Aa1'), 0);
+            assert.equal(fn('++A++'), 24);
+        });
+
+        it('calcMiddleNumbersOrSymbols', function () {
+            const fn = functions.calcMiddleNumbersOrSymbols;
+            assert.equal(fn('AbA'), 0);
+            assert.equal(fn('1234'), 4);
+            assert.equal(fn('A1@B3C+'), 6);
+        });
+
+        it('calcRequirements', function () {
+            const fn = functions.calcRequirements;
+            assert.equal(fn('Aa1$'), 0);
+            assert.equal(fn('AAAbbb123'), 8);
+            assert.equal(fn('AAbb12()'), 10);
+        });
+
+        it('calcLettersOnly', function () {
+            const fn = functions.calcLettersOnly;
+            assert.equal(fn('A'), -1);
+            assert.equal(fn('A1'), 0);
+            assert.equal(fn('A!'), 0);
+            assert.equal(fn('AbCdE'), -5);
+        });
+
+        it('calcNumbersOnly', function () {
+            const fn = functions.calcNumbersOnly;
+            assert.equal(fn('1'), -1);
+            assert.equal(fn('1A'), 0);
+            assert.equal(fn('1!'), 0);
+            assert.equal(fn('12345'), -5);
+        });
+
+        it('calcRepeatChars', function () {
+            const fn = functions.calcRepeatChars;
+            assert.equal(fn('A'), 0);
+            assert.equal(fn('AA'), -4);
+            assert.equal(fn('AAA'), -14);
+            assert.equal(fn('AAa'), -5);
+            assert.equal(fn('A1121A'), -7);
+            assert.equal(fn('aAAAa'), -17);
+        });
+
+        it('calcConsecutiveUppercase', function () {
+            const fn = functions.calcConsecutiveUppercase;
+            assert.equal(fn('A'), 0);
+            assert.equal(fn('AB'), -2);
+            assert.equal(fn('AAB'), -4);
+            assert.equal(fn('ABCD'), -6);
+            assert.equal(fn('A1AaB'), 0);
+        });
+
+        it('calcConsecutiveLowercase', function () {
+            const fn = functions.calcConsecutiveLowercase;
+            assert.equal(fn('a'), 0);
+            assert.equal(fn('ab'), -2);
+            assert.equal(fn('aab'), -4);
+            assert.equal(fn('abcd'), -6);
+            assert.equal(fn('A1aAb'), 0);
+        });
+
+        it('calcConsecutiveNumber', function () {
+            const fn = functions.calcConsecutiveNumber;
+            assert.equal(fn('1'), 0);
+            assert.equal(fn('12'), -2);
+            assert.equal(fn('123'), -4);
+            assert.equal(fn('1212'), -6);
+            assert.equal(fn('1Ab2A'), 0);
+        });
+
+        it('calcSequentialLetters', function () {
+            const fn = functions.calcSequentialLetters;
+            assert.equal(fn('abc'), -3);
+            assert.equal(fn('abcd'), -6);
+            assert.equal(fn('zyx'), -3);
+            assert.equal(fn('ABCDabc'), -6);
+            assert.equal(fn('ABCDEF1ABCD'), -12);
+        });
+
+        it('calcSequentialNumbers', function () {
+            const fn = functions.calcSequentialNumbers;
+            assert.equal(fn('012'), -3);
+            assert.equal(fn('7890'), -6);
+            assert.equal(fn('098'), -3);
+            assert.equal(fn('3210'), -6);
+            assert.equal(fn('012345A1234'), -12);
+        });
+
+        it('calcSequentialSymbols', function () {
+            const fn = functions.calcSequentialSymbols;
+            assert.equal(fn('~!@'), -3);
+            assert.equal(fn('()_+'), -6);
+            assert.equal(fn('+_)('), -6);
+            assert.equal(fn('<>?'), -3);
+            assert.equal(fn('{}|'), -3);
+            assert.equal(fn('A#$%^B&*()C'), -12);
         });
     });
 
 
-
     describe('password score', function () {
         const testSet = {
-            // '': 0,
-            // 'a': 3,
-            // '1': 3,
-            // '321': 4,
-            // 'Aa': 10,
-            // 'AA': 0,
-            // 'PassW0rd!': 72
+            '': 0,
+            'a': 3,
+            '1': 3,
+            '321': 4,
+            'Aa': 10,
+            'AA': 0,
+            'PassW0rd!': 72
         };
 
         Object.entries(testSet).forEach(e => {
             const [password, score] = e;
-            it(`'${password}' should be ${score}`, function () {
+            it(`strength of '${password}' === ${score}`, function () {
                 assert.equal(getPasswordStrength(password), score);
             });
         });
